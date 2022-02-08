d99974426d5f509a9d72d07fc392e72ed8e1c6c3
vagrant <tpirozzini@gmail.com>
2022-02-08 01:58:59 +0000

    add tail.js

---
+++ tail.js
@@ -0,0 +1,44 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// RETURN THE TAIL OF THE ARRAY
+const tail = function(arr) {
+  return arr.slice(1); //creates a shallow copy
+  // const newArr = arr.slice(1);
+  // return newArr;
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// Test Case 1: Check the returned array elements
+const result2 = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result2.length, 2); // ensure we get back two elements
+assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+// Test Case: Check the original array
+const words2 = [];
+tail(words2); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+// Test Case: Check the original array
+const words3 = ["Yo Yo"];
+tail(words3); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+const words4 = ["Yo Yo", "luigi", "mario", "link", "chun-li"];
+tail(words4); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
