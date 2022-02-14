2e06b8b8f33cefeb5dd54211e7e2224aec84d4ad
vagrant <tpirozzini@gmail.com>
2022-02-14 22:30:04 +0000

    refactor tail, assertEqual, head, tail, eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-
-const eqArrays = function(arr1, arr2) {
-  //check if arrays are the same length
-  if (arr1.length !== arr2.length) return false;
-  // check if array index's match
+const eqArrays = function(arr1, arr2) {  
+  if (arr1.length !== arr2.length) return false;  
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
     }
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
 
 
-// doesn't account for a situation when the array is in a different order //
-// const eqArrays = function (arr1, arr2) {
-//   return result = arr1.every(el => arr2.includes(el));
-// };
